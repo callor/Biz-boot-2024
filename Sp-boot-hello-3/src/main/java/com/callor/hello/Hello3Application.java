382a4443493fb9bc134d366a8ce21b6d2793219a
callor <callor@callor.com>
2024-05-22 11:51:54 +0900

    hello

---
+++ Sp-boot-hello-3/src/main/java/com/callor/hello/Hello3Application.java
@@ -0,0 +1,13 @@
+package com.callor.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hello3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hello3Application.class, args);
+	}
+
+}
---
+++ Sp-boot-hello-3/src/test/java/com/callor/hello/Hello3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.callor.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hello3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
