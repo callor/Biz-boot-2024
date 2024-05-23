fba268dd9ea2fe57b5be6d0b0436b74a129c0629
callor <callor@callor.com>
2024-05-23 09:30:10 +0900

    Book Project 시작

---
+++ Sp-boot-book-1/src/main/java/com/callor/book/BookProjectApplication.java
@@ -0,0 +1,13 @@
+package com.callor.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookProjectApplication.class, args);
+	}
+
+}
