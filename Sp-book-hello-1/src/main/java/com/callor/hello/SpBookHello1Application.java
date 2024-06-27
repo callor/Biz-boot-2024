65fc938ac4a9e2cad6fd38d4649760e0f60dbc17
callor <callor@callor.com>
2024-06-27 17:25:21 +0900

    hello

---
+++ Sp-book-hello-1/src/main/java/com/callor/hello/SpBookHello1Application.java
@@ -0,0 +1,13 @@
+package com.callor.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpBookHello1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpBookHello1Application.class, args);
+	}
+
+}
---
+++ Sp-book-hello-1/src/test/java/com/callor/hello/SpBookHello1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.callor.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpBookHello1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
